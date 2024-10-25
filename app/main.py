72a040c60a25873893ba0c56cec462c878851df2
Leon <leonjs02@protonmail.com>
2024-10-25 20:30:59 +0100

    Added Flask to requirements.

---
+++ app/main.py
@@ -0,0 +1,35 @@
+"""The main endpoints for the app's front-end."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Homepage for the app."""
+    return {"message": "VinylVault Search"}, 200
+
+
+@app.route("/display_search", methods=["POST"])
+def display_search():
+    """Displays search results of a particular query (made through a POST request)."""
+    return {"message": "Displaying search results..."}, 200
+
+
+@app.route("/collection", methods=["GET"])
+def collection():
+    """Displays the user's entire collection within their database."""
+    return {"message": "Displaying all albums in collection..."}, 200
+
+
+@app.route("/collection/<int:album_id>", methods=["GET"])
+def display_album(album_id: int):
+    """Displays an album of a particular ID within the user's collection."""
+    return {"message": "Displaying the chosen album..."}, 200
+
+
+if __name__ == "__main__":
+    app.config["DEBUG"] = True
+    app.config["TESTING"] = True
+    app.run(port=8080, debug=True)
