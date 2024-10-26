4687fe7ee063e37fd059bc13d29ec6bc3f5ac83b
Leon <leonjs02@protonmail.com>
2024-10-26 17:02:24 +0100

    Added functions to extract artist details and genres.

--- app/extract_spotify.py
+++ app/extract_spotify.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 import requests as req
 
-from endpoints import SEARCH_ENDPOINT
+from endpoints import SEARCH_ENDPOINT, ARTIST_ENDPOINT
 
 TIMEOUT = 10
 
@@ -16,6 +16,23 @@ def search_album(query: str, access_token: str) -> list[dict]:
     if response.status_code == 200:
         return response.json()["albums"]["items"]
     raise ConnectionError(f"Failed to retrieve data. Code: {response.status_code}")
+
+def call_get_artist_endpoint(artist_id: str, access_token: str) -> dict:
+    """Makes an API call to the artist_id endpoint. Returns the response dict."""
+    url = f"{ARTIST_ENDPOINT}{artist_id}"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = req.get(url, headers=headers, timeout=TIMEOUT)
+    if response.status_code == 200:
+        return response.json()
+    raise ConnectionError(f"Failed to retrieve data. Code: {response.status_code}")
+
+def parse_artist_from_api(response: dict) -> dict:
+    """Removes unnecessary details from an get artist API response."""
+    return{
+        'spotify_id': response['id'],
+        'name': response['name'],
+        'genres': response['genres']
+    }
 
 def parse_release_date(release_date: str, release_date_precision: str) -> str:
     """Returns a standardised release date string in
@@ -42,6 +59,10 @@ def get_image_url(album: dict) -> str:
     images = [x['url'] for x in album['images']]
     return images[0] if images else None
 
+def get_artists_from_artist_ids(artist_ids: list, access_token: str) -> list[dict]:
+    """Returns a list of artist dicts from an album ID."""
+    responses = [call_get_artist_endpoint(x, access_token) for x in artist_ids]
+    return [parse_artist_from_api(response) for response in responses]
 
 def parse_search_results(albums: list[dict]) -> list[dict]:
     """Strips unnecessary information from the album dictionaries.
