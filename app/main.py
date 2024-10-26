421c99768ed5ec5db8da53ec571c5450e171351d
Leon <leonjs02@protonmail.com>
2024-10-26 22:09:30 +0100

    Added functions.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 """The main endpoints for the app's front-end."""
+from os import environ as ENV
 
-from flask import Flask
+from flask import Flask, request
+from dotenv import load_dotenv
+
+from extract_spotify import (search_album, parse_search_results, add_album,
+                             call_get_album_endpoint, parse_album_from_api)
+from db_utils import get_album_by_id
+from authorisation.access_manager import generate_and_replace
 
 app = Flask(__name__)
+load_dotenv()
 
 
 @app.route("/", methods=["GET"])
@@ -14,13 +22,19 @@ def index():
 @app.route("/display_search", methods=["POST"])
 def display_search():
     """Displays search results of a particular query (made through a POST request)."""
-    return {"message": "Displaying search results..."}, 200
+    query = request.form.get("search_query")
+    print(query)
+    results = search_album(query, ENV['ACCESS_TOKEN'])
+    parsed_results = parse_search_results(results)
+    return parsed_results, 200
 
 
-@app.route("/add/<str:spotify_album_id>", methods=["POST"])
+@app.route("/add/<string:spotify_album_id>", methods=["POST"])
 def add(spotify_album_id: str):
     """Adds an album of a specific Spotify ID to the user's collection."""
+    add_album(spotify_album_id, ENV['ACCESS_TOKEN'])
     return {"message": "Adding album to collection..."}, 200
+
 
 @app.route("/collection", methods=["GET"])
 def collection():
@@ -31,7 +45,9 @@ def collection():
 @app.route("/collection/<int:album_id>", methods=["GET"])
 def display_album(album_id: int):
     """Displays an album of a particular ID within the user's collection."""
-    return {"message": "Displaying the chosen album..."}, 200
+    # Get album by ID.
+    album_info = get_album_by_id(album_id)
+    return {}, 200
 
 
 if __name__ == "__main__":
