814684d6ee515b6e6737fb126635224d0884345e
Leon <leonjs02@protonmail.com>
2024-10-26 17:26:02 +0100

    Added function to insert genre.

--- app/db_utils.py
+++ app/db_utils.py
@@ -32,3 +32,14 @@ def insert_artist(spotify_artist_id: str, artist_name: str) -> int:
             cur.execute(stmt, (spotify_artist_id, artist_name,))
             artist_id = cur.fetchone()['artist_id']
     return artist_id
+
+
+def insert_genre(genre_name: str) -> int:
+    """Inserts an genre into the database, returning its ID."""
+    stmt = """INSERT INTO genre(genre_name)
+    VALUES (%s) RETURNING genre_id;"""
+    with get_connection() as conn:
+        with get_cursor(conn) as cur:
+            cur.execute(stmt, (genre_name,))
+            genre_id = cur.fetchone()['genre_id']
+    return genre_id
