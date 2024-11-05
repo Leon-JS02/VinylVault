196f203eb6560f122f2a9a9d42133f090c789eb5
Leon <leonjs02@protonmail.com>
2024-11-05 19:18:12 +0000

    Added second column to dashboard for genre chart.

--- app/analytics/dashboard.py
+++ app/analytics/dashboard.py
@@ -2,12 +2,20 @@
 import streamlit as st
 from dotenv import load_dotenv
 
-from analytics_utils import get_decade_counts
-from graphs import decade_chart
+from analytics_utils import get_decade_counts, get_genre_counts
+from graphs import decade_chart, genre_chart
 
 if __name__ == "__main__":
     load_dotenv()
     st.set_page_config(layout="wide")
     decade_data = get_decade_counts()
-    chart = decade_chart(decade_data)
-    st.altair_chart(chart, use_container_width=False)
+    genre_data = get_genre_counts()
+
+    cols = st.columns([1, 1])
+    decade_pie_chart = decade_chart(decade_data)
+    genre_bar_chart = genre_chart(genre_data)
+
+    with cols[0]:
+        st.altair_chart(decade_pie_chart, use_container_width=False)
+    with cols[1]:
+        st.altair_chart(genre_bar_chart, use_container_width=True)
